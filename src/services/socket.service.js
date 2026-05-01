bdf81869aad22280b952ef3535864c0acc6513c4
meetshopno <meet.shopno@gmail.com>
2026-05-01 15:50:44 +0530

    socket path dynamic

--- src/services/socket.service.js
+++ src/services/socket.service.js
@@ -7,7 +7,7 @@ const adminSockets = new Set(); // set of admin socketIds
 
 const init = (server) => {
   io = new Server(server, {
-    path: '/api/api/socket.io',
+    path: SOCKET_PATH,
     cors: {
       origin: function(origin, callback) {
         const allowedOrigins = [
@@ -35,7 +35,7 @@ const init = (server) => {
       methods: ['GET', 'POST'],
       credentials: true,
     },
-    transports: ['polling'],
+    transports: ['websocket','polling'],
     pingTimeout: 60000,       // 60s — wait before declaring connection dead
     pingInterval: 25000,      // 25s — heartbeat every 25s
     upgradeTimeout: 30000,    // 30s — time to upgrade from polling to websocket
