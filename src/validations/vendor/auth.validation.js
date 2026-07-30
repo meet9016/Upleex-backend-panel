476735a5bdd828ecff75d10ae342b272fb001ed9
Radhi <digi.radhi@gmail.com>
2026-07-30 15:36:16 +0530

    done

--- src/validations/vendor/auth.validation.js
+++ src/validations/vendor/auth.validation.js
@@ -4,7 +4,12 @@ const businessRegister = {
   body: Joi.object().keys({
     full_name: Joi.string().required(),
     business_name: Joi.string().required(),
-    email: Joi.string().email().required(),
+    email: Joi.string().email().custom((value, helpers) => {
+      if (!value.endsWith('@gmail.com')) {
+        return helpers.message('Email must be a @gmail.com address');
+      }
+      return value;
+    }).required(),
     number: Joi.string().pattern(/^[0-9]{10,15}$/).required(),
     alternate_number: Joi.string().pattern(/^[0-9]{10,15}$/).allow('', null).optional(),
     country: Joi.string().required(),
